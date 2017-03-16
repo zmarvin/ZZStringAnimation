a195d56db5c3a7ce79c6c062885da77b8c382a3b
ZzComeOn <zz890620@gmail.com>
2017-03-16 19:37:41 +0800

    upgrade

--- ZZStringAnimationDemo/ViewController.m
+++ ZZStringAnimationDemo/ViewController.m
@@ -38,46 +38,46 @@
     [btn1 addTarget:self action:@selector(click1) forControlEvents:UIControlEventTouchUpInside];
 
     
-//    UILabel *label = [UILabel new];
-//    label.frame = CGRectMake(50, 100, 280, 100);
-//    label.text = @"昔人已乘黄鹤去，此地空余黄鹤楼。";
-//    label.textColor = [UIColor redColor];
-//    [self.view addSubview:label];
-//    _label = label;
-//
-//    
-//    UILabel *label2 = [UILabel new];
-//    label2.frame = CGRectMake(50, 150, 280, 100);
-//    label2.text = @"黄鹤一去不复返，白云千载空悠悠。";
-//    label2.textColor = [UIColor redColor];
-//    [self.view addSubview:label2];
-//    _label2 = label2;
-//
-//    
-//    UILabel *label3 = [UILabel new];
-//    label3.frame = CGRectMake(50, 200, 280, 100);
-//    label3.text = @"晴川历历汉阳树，芳草萋萋鹦鹉洲。";
-//    label3.textColor = [UIColor redColor];
-//    [self.view addSubview:label3];
-//    _label3 = label3;
-//
-//    
-//    UILabel *label4 = [UILabel new];
-//    label4.frame = CGRectMake(50, 250, 280, 100);
-//    label4.text = @"日暮乡关何处是，烟波江上使人愁。";
-//    label4.textColor = [UIColor redColor];
-//    [self.view addSubview:label4];
-//    _label4 = label4;
+    UILabel *label = [UILabel new];
+    label.frame = CGRectMake(50, 100, 280, 100);
+    label.text = @"昔人已乘黄鹤去，此地空余黄鹤楼。";
+    label.textColor = [UIColor redColor];
+    [self.view addSubview:label];
+    _label = label;
+
+    
+    UILabel *label2 = [UILabel new];
+    label2.frame = CGRectMake(50, 150, 280, 100);
+    label2.text = @"黄鹤一去不复返，白云千载空悠悠。";
+    label2.textColor = [UIColor redColor];
+    [self.view addSubview:label2];
+    _label2 = label2;
+
+    
+    UILabel *label3 = [UILabel new];
+    label3.frame = CGRectMake(50, 200, 280, 100);
+    label3.text = @"晴川历历汉阳树，芳草萋萋鹦鹉洲。";
+    label3.textColor = [UIColor redColor];
+    [self.view addSubview:label3];
+    _label3 = label3;
+
+    
+    UILabel *label4 = [UILabel new];
+    label4.frame = CGRectMake(50, 250, 280, 100);
+    label4.text = @"日暮乡关何处是，烟波江上使人愁。";
+    label4.textColor = [UIColor redColor];
+    [self.view addSubview:label4];
+    _label4 = label4;
     
     
-//    UILabel *label5 = [UILabel new];
-//    label5.frame = CGRectMake(30, 320, 355, 100);
-//    label5.text = @"君不见黄河之水天上来，奔流到海不复回。\n君不见高堂明镜悲白发，朝如青丝暮成雪。\n人生得意须尽欢，莫使金樽空对月。\n天生我材必有用，千金散尽还复来。";
-//    label5.numberOfLines = 0;
-//    label5.textColor = [UIColor redColor];
-//    [self.view addSubview:label5];
-//    _label5 =label5;
-//    
+    UILabel *label5 = [UILabel new];
+    label5.frame = CGRectMake(30, 320, 355, 100);
+    label5.text = @"君不见黄河之水天上来，奔流到海不复回。\n君不见高堂明镜悲白发，朝如青丝暮成雪。\n人生得意须尽欢，莫使金樽空对月。\n天生我材必有用，千金散尽还复来。";
+    label5.numberOfLines = 0;
+    label5.textColor = [UIColor redColor];
+    [self.view addSubview:label5];
+    _label5 =label5;
+    
     UILabel *label6 = [UILabel new];
     label6.frame = CGRectMake(30, 400, 355, 100);
     label6.text = @"String animation, currently only supports UILabel. 字符串动画，目前只支持UILabel。";
@@ -90,29 +90,29 @@
 
 - (void)click1 {
     
-//    ZZLightAnimation *lightAnimation = [ZZLightAnimation new];
-//    lightAnimation.color = [UIColor whiteColor];
-//    lightAnimation.duration = 1;
-//    [_label zz_startAnimation:lightAnimation];
-//    
-//    ZZEnlargeAnimation *enlargeAnimation = [ZZEnlargeAnimation new];
-//    enlargeAnimation.duration = 5;
-//    enlargeAnimation.enlargeMultiple = 3;
-//    [_label2 zz_startAnimation:enlargeAnimation];
-//    
-//    ZZDrawAnimation *drawAnimation = [ZZDrawAnimation new];
-//    drawAnimation.duration = 5;
-//    [_label3 zz_startAnimation:drawAnimation];
-//    
-//    ZZGradualAnimation *gradualAnimation = [ZZGradualAnimation new];
-//    gradualAnimation.duration = 5;
-//    [_label4 zz_startAnimation:gradualAnimation];
+    ZZLightAnimation *lightAnimation = [ZZLightAnimation new];
+    lightAnimation.color = [UIColor whiteColor];
+    lightAnimation.duration = 1;
+    [_label zz_startAnimation:lightAnimation];
     
-//    ZZWaveAnimation *waveAnimation = [ZZWaveAnimation new];
-//    waveAnimation.duration = 5;
-//    waveAnimation.waveHeight = 20;
-//    [_label5 zz_startAnimation:waveAnimation];
-//
+    ZZEnlargeAnimation *enlargeAnimation = [ZZEnlargeAnimation new];
+    enlargeAnimation.duration = 5;
+    enlargeAnimation.enlargeMultiple = 3;
+    [_label2 zz_startAnimation:enlargeAnimation];
+    
+    ZZDrawAnimation *drawAnimation = [ZZDrawAnimation new];
+    drawAnimation.duration = 5;
+    [_label3 zz_startAnimation:drawAnimation];
+    
+    ZZGradualAnimation *gradualAnimation = [ZZGradualAnimation new];
+    gradualAnimation.duration = 5;
+    [_label4 zz_startAnimation:gradualAnimation];
+    
+    ZZWaveAnimation *waveAnimation = [ZZWaveAnimation new];
+    waveAnimation.duration = 5;
+    waveAnimation.waveHeight = 20;
+    [_label5 zz_startAnimation:waveAnimation];
+
     ZZPathAnimation *pathAnimation = [ZZPathAnimation new];
     pathAnimation.duration = 5;
     pathAnimation.path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(25, 200, 300, 300)];
